97ec71a2245fe3c3dc88c2243a8a5ec76f2ce9af
hpord <buho013@gmail.com>
2025-04-04 10:12:46 -0500

    Ejercicio 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -14,12 +14,20 @@ const Part = ({name, exercises}) => (
   </p>
 )
 
-//const Total = (props) => <p>Number of exercises {props.total}</p>
+const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const Course = ({course}) => (
   <>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises +
+          course.parts[3].exercises
+        }
+    />
   </>
 )
 
