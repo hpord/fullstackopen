d24f0868b45d5e59508c5439be4449c9a61375f1
hpord <buho013@gmail.com>
2025-04-04 10:06:48 -0500

    Ejercicio 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,26 @@
+const Header = ({course}) => <h1>{course}</h1>
+
+const Content = ({parts}) => (
+  <>
+    {parts.map(part =>
+        <Part key={part.id} name={part.name} exercises={part.exercises}/>
+    )}
+  </>
+)
+
+const Part = ({name, exercises}) => (
+  <p>
+    {name} {exercises}
+  </p>
+)
+
+//const Total = (props) => <p>Number of exercises {props.total}</p>
+
+const Course = ({course}) => (
+  <>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+  </>
+)
+
+export default Course
